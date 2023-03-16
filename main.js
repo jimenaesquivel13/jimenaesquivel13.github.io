a8bef7f7ea32ab1e4ff666a7a239ce30bcb97a82
jimenaesquivel13 <125763525+jimenaesquivel13@users.noreply.github.com>
2023-03-15 23:52:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('<span style="color:#ff0f60";> Soy estudiante de Desarrollo de Software Multiplataforma y creo sitios web.</span>')
+  .typeString('<span style="color:#ff0f60";> Soy estudiante en Desarrollo de Software Multiplataforma y creo sitios web.</span>')
   .pauseFor(200)
   .deleteChars(10)
   
